6de754e34b15001dbd811d641700c2300130594c
huangkata <332106187@qq.com>
2019-12-15 05:09:33 -0800

    testst aad a

--- helloworld.c
+++ helloworld.c
@@ -1,4 +1,13 @@
-C\
-ggggjkgsa fr a tst 
-C
-p
+#include "stdio.h"
+#include "string.h"
+
+void main()
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("test i %d \n", i);
+	}
+	return;
+}
+
